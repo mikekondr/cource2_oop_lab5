1b352beb45365013b8a7d921059082c62f784235
mikekondr <mike.kondr@gmail.com>
2023-10-30 23:20:24 +0200

    init

---
+++ task1/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace task1
+{
+
+    public abstract class Parent
+    {
+        public abstract string Info();
+
+        public abstract float Method();
+    }
+
+    public class Child1 : Parent
+    {
+        float pole1, pole2;
+
+        public Child1(float pole1, float pole2)
+        {
+            this.pole1 = pole1;
+            this.pole2 = (float)Math.Round(pole2, 2);
+        }
+
+        public override string Info()
+        {
+            return $"Дохід: {pole1:F2}, податок: {pole2:F2}";
+        }
+
+        public override float Method()
+        {
+            return (float)Math.Round(pole1 * pole2 / 100, 2);
+        }
+    }
+
+    public class Child2 : Parent
+    {
+        float pole3, pole4, pole5, pole6;
+
+        public Child2(float pole3, float pole4, float pole5, float pole6)
+        {
+            this.pole3 = pole3;
+            this.pole4 = pole4;
+            this.pole5 = pole5;
+            this.pole6 = (float)Math.Round(pole6, 2);
+        }
+
+        public override string Info()
+        {
+            return $"Виручка: {pole3:F2}, собівартість: {pole4:F2}, " +
+                $"дод.витрати: {pole5:F2}, ставка податку: {pole6:F2}";
+        }
+
+        public override float Method()
+        {
+            float sum = (float)Math.Round((pole3 - pole4 - pole5) * pole6 / 100, 2);
+            return (sum >= 0) ? sum : 0;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine();
+
+                if (rnd.Next(1, 100) <= 50)
+                {
+                    Child1 o = new Child1(
+                        rnd.Next(1, 100000),
+                        rnd.NextSingle() * rnd.Next(0, 100)
+                        );
+                    Console.WriteLine(o.Info());
+                    Console.WriteLine($"Сума податку: {o.Method():F2}");
+                }
+                else
+                {
+                    Child2 o = new Child2(
+                        rnd.Next(1,100000),
+                        rnd.Next(1,50000),
+                        rnd.Next(1, 10000),
+                        rnd.NextSingle() * rnd.Next(0, 100)
+                        );
+                    Console.WriteLine(o.Info());
+                    Console.WriteLine($"Сума податку: {o.Method():F2}");
+                }
+            }
+        }
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,125 @@
+﻿namespace task2;
+
+interface IParent
+{
+    string Info();
+    float Square();
+    float Value();
+}
+
+class Child1 : IParent
+{
+    float pole1, pole2, pole3;
+
+    public Child1(float pole1, float pole2, float pole3)
+    {
+        this.pole1 = pole1;
+        this.pole2 = pole2;
+        this.pole3 = pole3;
+    }
+
+    public string Info()
+    {
+        return $"Паралелепіпед: a = {pole1:F3}, b = {pole2:F3}, c = {pole3:F3}";
+    }
+
+    public float Square()
+    {
+        return 2 * (pole1 * pole2 + pole2 * pole3 + pole3 * pole1);
+    }
+
+    public float Value()
+    {
+        return pole1 * pole2 * pole3;
+    }
+}
+
+class Child2 : IParent
+{
+    float pole4, pole5;
+
+    public Child2(float pole4, float pole5)
+    {
+        this.pole4 = pole4;
+        this.pole5 = pole5;
+    }
+
+    public string Info()
+    {
+        return $"Конус: R = {pole4:F3}, h = {pole5:F3}";
+    }
+
+    public float Square()
+    {
+        return (float)Math.PI * pole4 * (pole4 + (float)Math.Sqrt(pole4*pole4 + pole5*pole5));
+    }
+
+    public float Value()
+    {
+        return ((float)Math.PI * pole4*pole4 * pole5) / 3f;
+    }
+}
+
+class Child3 : IParent
+{
+    float pole6;
+
+    public Child3(float pole6)
+    {
+        this.pole6 = pole6;
+    }
+
+    public string Info()
+    {
+        return $"Куля: R = {pole6:F3}";
+    }
+
+    public float Square()
+    {
+        return 4 * (float)Math.PI * pole6 * pole6;
+    }
+
+    public float Value()
+    {
+        return ( 4 * (float)Math.PI * pole6 * pole6 * pole6 ) / 3f;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IParent o;
+        Random rnd = new Random();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine();
+
+            int fig = rnd.Next(1, 9);
+            if (fig >= 1 && fig < 3)
+            {
+                o = new Child1(
+                    rnd.NextSingle() * rnd.Next(1, 10),
+                    rnd.NextSingle() * rnd.Next(1, 10),
+                    rnd.NextSingle() * rnd.Next(1, 10)
+                    );
+            }else if (fig >= 3 && fig < 6)
+            {
+                o = new Child2(
+                    rnd.NextSingle() * rnd.Next(1, 10),
+                    rnd.NextSingle() * rnd.Next(1, 10)
+                    );
+            }else
+            {
+                o = new Child3(
+                    rnd.NextSingle() * rnd.Next(1, 10)
+                    );
+            }
+
+            Console.WriteLine(o.Info());
+            Console.WriteLine($"S = {o.Square():F3}, V = {o.Value():F3}");
+        }
+    }
+}
+
